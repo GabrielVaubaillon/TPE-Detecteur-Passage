eaa3663173a7320cb3a9adef02b333a04a083019
LeRagib <gabriel.vaubaillon@gmail.com>
2019-11-08 16:00:58 +0100

    le programme de simulation fonctionne. Il va falloir que je nettoie tout cette bidouille, et que je fasse un petit affichage, des valeurs et de la situation

--- config.py
+++ config.py
@@ -1,1 +1,38 @@
- 
+
+#Les positions sont exprimées dans un repère cartésien avec x horizontal vers
+#la droite et y vertical vers le bas
+#Les valeurs des coordonnées sont en centimetres
+posCapteur = (10,5)
+
+#On suppose que les diodes ont toujours la même position en x.
+#On choisit à priori tout le temps l'origine à la diode 1
+posDiode1 = (0,0)
+
+posDiode2 = (0,10)
+
+#La vitesse est exprimée en m/s. La seule vitesse sera selon y
+vitesseObstacle = 1.0
+#La largeur de l'obstacle qui passe par la porte
+tailleObstacle = 30
+
+#Les intensités des diodes sont les valeurs les plus difficiles à obtenir
+#Il faut prendre en compte le cone de dispersion. On simplifie grandement cette
+# étape en prenant un capteur ponctuel. Nous avons par conséquent une seule valeur
+# d'intensité reçue par le capteur pour chaque diode
+intensiteDiode1 = 8.0
+
+intensiteDiode2 = 4.0
+
+#Nombre de secondes entre chaque calcul (float)
+dt = 0.5
+
+#Propre au programme :
+from pygame.locals import *
+
+#Taille de la fenetre (plus tard calculé en fonction des points donnés)
+#Les valeurs sont en pixel
+largeur = 1000
+hauteur = 1000
+
+keyEntree = K_UP
+keySortie = K_DOWN
--- simulation.py
+++ simulation.py
@@ -1,1 +1,88 @@
- 
+from config import *
+import pygame
+from pygame.locals import *
+
+
+#Initialisations des valeurs :
+xCentre = (posCapteur[0] + posDiode1[0]) / 2
+#On prend : entree : L'obstacle descend (vitesse selon y positif)
+#           sortie : L'obstacle monte (vitesse selon y négatif)
+yDepartEntree = min(posDiode1[1] , posDiode2[1] , posDiode2[1]) - tailleObstacle - 10
+yDepartSortie = max(posDiode1[1] , posDiode2[1] , posDiode2[1]) + 10
+
+yObstacleRayon1 = (posCapteur[1] / posCapteur[0]) * xCentre
+yObstacleRayon2 = ((posCapteur[1] - posDiode2[1]) / posCapteur[0]) * xCentre + posDiode2[1]
+
+valeurRef = intensiteDiode1 + intensiteDiode2
+
+ecartDiode = posDiode2[1] #Diode 1 est l'origine
+dtParPassage = (ecartDiode + tailleObstacle + 10) / (vitesseObstacle * dt)
+print(dtParPassage)
+
+passageEnCours = False
+
+#Fonctions :
+def deplaceObstacle(vitesse, dt, posObstacle):
+    return (posObstacle[0], posObstacle[1] + dt * vitesse)
+
+def intensiteCapteur(posObstacle , tailleObstacle, intensiteDiode1 , yObstacleRayon1,intensiteDiode2 , yObstacleRayon2):
+    yHaut = posObstacle[1]
+    yBas = posObstacle[1] + tailleObstacle
+    valeur = 0
+    #Rappel : l'axe y est vers le bas
+    if not (yObstacleRayon1 > yHaut and yObstacleRayon1 < yBas):
+        valeur += intensiteDiode1
+
+    if not (yObstacleRayon2 > yHaut and yObstacleRayon2 < yBas):
+        valeur += intensiteDiode2
+
+    return valeur
+
+
+#Initialisation de l'IHM
+pygame.init()
+
+fenetre = pygame.display.set_mode((largeur, hauteur))
+
+skinDiode1 = pygame.Surface((5,5))
+skinDiode1.fill((255,0,0))
+skinDiode2 = pygame.Surface((5,5))
+skinDiode2.fill((255,0,0))
+skinCapteur = pygame.Surface((5,5))
+skinCapteur.fill((0,255,0))
+
+#BouclePrincipale
+continuer = True
+while continuer:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            continuer = False
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                continuer = False
+
+            if not passageEnCours:
+                if event.key == keyEntree:
+                    temps = 0 #faux temps, temps de la simulation, en dt
+                    passageEnCours = True
+                    valeurs = []
+                    vitesse = vitesseObstacle
+                    posObstacle = (xCentre, yDepartEntree)
+                    print("entrée")
+
+                if event.key == keySortie:
+                    temps = 0
+                    passageEnCours = True
+                    valeurs = []
+                    vitesse = - vitesseObstacle
+                    posObstacle = (xCentre, yDepartSortie)
+                    print("sortie")
+
+    if passageEnCours:
+        posObstacle = deplaceObstacle(vitesse,dt,posObstacle)
+        valeur = intensiteCapteur(posObstacle , tailleObstacle, intensiteDiode1 , yObstacleRayon1,intensiteDiode2 , yObstacleRayon2)
+        valeurs.append(valeur)
+        temps += 1
+        if temps >= dtParPassage:
+            print(valeurs)
+            passageEnCours = False
